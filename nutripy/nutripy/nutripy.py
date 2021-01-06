4ab9e9b9970b91cd478f1678857bd98b4dfc53f3
yfe404 <yfe.github@protonmail.com>
2021-01-06 18:21:13 +0100

    use enums

--- nutripy/nutripy/nutripy.py
+++ nutripy/nutripy/nutripy.py
@@ -1,44 +1,30 @@
+from enum import Enum
+
+class Gender(Enum):
+    MALE = 5
+    FEMALE = -161
+
+class Activity(Enum):
+    SEDENTARY = 1.2
+    LIGHTLY_ACTIVE = 1.375
+    MODERATELY_ACTIVE = 1.55
+    VERY_ACTIVE = 1.725
+    EXTRA_ACTIVE = 1.9 
+
+class Goal(Enum):
+    LOSS_1000 = -1000
+    LOSS_500 = -500
+    MAINTAIN = 0
+    GAIN_500 = 500
+    GAIN_1000 = 1000
+
+    
 class Nutripy(object):
- 
-    def _get_gender_factor(self, gender):
-        if gender == "male":
-            return 5
-        elif gender == "female":
-            return -161
-
-    def _get_activity_factor(self, activity):
-        if activity == "sedentary":
-            return 1.2
-        elif activity == "lightly_active":
-            return 1.375
-        elif activity == "moderately_active":
-            return 1.55
-        elif activity == "very_active":
-            return 1.725
-        elif activity == "extra_active":
-            return 1.9 
-
-    def _get_goal_factor(self, goal):
-        if goal == "loss_1000":
-            return -1000
-        elif goal == "loss_500":
-            return -500
-        elif goal == "maintain":
-            return 0
-        elif goal == "gain_500":
-            return 500
-        elif goal == "gain_1000":
-            return 1000
-
     def _get_basal_metabolic_rate(self, age, weight, height, gender):
-        gender_factor = self._get_gender_factor(gender)
-        bmr = (10 * weight) + (6.25 * height) - (5 * age) + gender_factor
+        bmr = (10 * weight) + (6.25 * height) - (5 * age) + gender.value
         return bmr
-
         
     def get_daily_needs(self, age, weight, height, gender, activity, goal):
         bmr = self._get_basal_metabolic_rate(age, weight, height, gender)
-        activity_factor = self._get_activity_factor(activity)
-        goal_factor = self._get_goal_factor(goal)
-        daily_needs = (bmr * activity_factor) + goal_factor
+        daily_needs = (bmr * activity.value) + goal.value
         return daily_needs
--- nutripy/nutripy/tests/test_nutripy.py
+++ nutripy/nutripy/tests/test_nutripy.py
@@ -1,49 +1,16 @@
 import unittest
-from ..nutripy import Nutripy
+from ..nutripy import Nutripy, Gender, Activity, Goal
 
 
 class TddNutripy(unittest.TestCase):
  
-    def test_correct_gender_factor(self):
-        nut = Nutripy()
-        gender_factor = nut._get_gender_factor("male")
-        self.assertEqual(gender_factor, 5)
-        gender_factor = nut._get_gender_factor("female")
-        self.assertEqual(gender_factor, -161)
-
-    def test_correct_activity_factor(self):
-        nut = Nutripy()
-        activity_factor = nut._get_activity_factor("sedentary")
-        self.assertEqual(activity_factor, 1.2)
-        activity_factor = nut._get_activity_factor("lightly_active")
-        self.assertEqual(activity_factor, 1.375)
-        activity_factor = nut._get_activity_factor("moderately_active")
-        self.assertEqual(activity_factor, 1.55)
-        activity_factor = nut._get_activity_factor("very_active")
-        self.assertEqual(activity_factor, 1.725)
-        activity_factor = nut._get_activity_factor("extra_active")
-        self.assertEqual(activity_factor, 1.9)
-
-    def test_correct_goal_factor(self):
-        nut = Nutripy()
-        goal_factor = nut._get_goal_factor("loss_1000")
-        self.assertEqual(goal_factor, -1000)
-        goal_factor = nut._get_goal_factor("loss_500")
-        self.assertEqual(goal_factor, -500)
-        goal_factor = nut._get_goal_factor("maintain")
-        self.assertEqual(goal_factor, 0)
-        goal_factor = nut._get_goal_factor("gain_500")
-        self.assertEqual(goal_factor, 500)
-        goal_factor = nut._get_goal_factor("gain_1000")
-        self.assertEqual(goal_factor, 1000)
-
     def test_correct_basal_metabolic_rate(self):
         nut = Nutripy()
         
         age = 25
         weight = 60
         height = 180
-        gender = "male"
+        gender = Gender.MALE
         bmr = nut._get_basal_metabolic_rate(age, weight, height, gender)
         self.assertEqual(bmr, 1605)
 
@@ -53,9 +20,9 @@ class TddNutripy(unittest.TestCase):
         age = 25
         weight = 60
         height = 180
-        gender = "male"
-        activity = "sedentary"
-        goal = "loss_1000"
+        gender = Gender.MALE
+        activity = Activity.SEDENTARY
+        goal = Goal.LOSS_1000
         daily_needs = nut.get_daily_needs(age, weight, height, gender, activity, goal)
         self.assertEqual(daily_needs, 926)
         
