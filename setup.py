c7b0f1c4cdb0fcbf2788cfcaf1aa70cd643401c8
yfe404 <yfe.github@protonmail.com>
2021-02-25 11:10:40 +0100

    v 0.7.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
     name="nutripy", 
-    version="0.6.2",
+    version="0.7.0",
     author="Yann Feunteun",
     author_email="yann.feunteun@protonmail.com",
     description="A Python module for computing calorie requirements for \
